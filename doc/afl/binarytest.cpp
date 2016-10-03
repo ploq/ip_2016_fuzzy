aa6f7738fe7b101c67bfca466f5b069e3b928ccb
Oscar Holm <oscho707@student.liu,se>
2016-10-03 15:20:08 +0200

    AFL_LOOP() tester.

--- doc/afl/binarytest.cpp
+++ doc/afl/binarytest.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <sstream>
+#include <fstream>
 
 using namespace std;
 
@@ -9,29 +9,24 @@ struct parameters {
   char randtype;
 };
 
+void printParam(const struct parameters &params, ofstream &outputfile) {
+  outputfile << "CYCLES: " << params.cycles << endl << "SEED: " << params.seed << endl << "RANDTYPE: " << (int)params.randtype << endl;
+}
+
 int main(int argc, char** argv) {
-  if (argc != 2)
-    {
-      cout << "FEL PARAMETRAR!" << endl;
-      return 1;
-    }
-
-  char buffer[1000];
-  stringstream ss;
-  
-  ss << argv[1];
-  ss >> buffer;
-  //ifstream inputfile (argv[1], ios::in | ios::binary);
-
-  parameters params;
-  params = *((parameters*)buffer);
   ofstream outputfile ("outputfile.txt", ios_base::app);
   
-  while ( __AFL_LOOP(10) ) {
-    outputfile << "CYCLES: " << params.cycles << endl << "SEED: " << params.seed << endl << "RANDTYPE: " << (int)params.randtype << endl;
+  while ( __AFL_LOOP(100000) ) {
+    string buffer;
+    cin >> buffer;
+
+    parameters params;
+    params = *((parameters*)buffer.c_str());
+
+    printParam(params, outputfile);
   }
+
   outputfile.close();
-  cout << buffer << endl;
   
   return 0;
 }
--- doc/afl/run.sh
+++ doc/afl/run.sh
@@ -1,5 +1,15 @@
 rm output/out.min
 rm outputfile.txt
-rm input/~testfile.bin
-afl-fuzz -i input/ -o output/ -n ./a.out @@
-less outputfile.txt
+touch outputfile.txt
+rm -f input/*
+rm -f raw_input/*
+#generate testcase
+dd if=/dev/random of=raw_input/testfile1.bin bs=64 count=1
+#minimize testcases
+afl-cmin -i raw_input/ -o input/ ./a.out 
+~/afl-2.35b/afl-clang-fast++ binarytest.cpp
+
+#screen afl-fuzz -i input -o sync_dir/ -M fuzzer1 ./a.out
+#screen afl-fuzz -i input -o sync_dir/ -S fuzzer2 ./a.out
+afl-fuzz -i input/ -o output/ ./a.out
+
