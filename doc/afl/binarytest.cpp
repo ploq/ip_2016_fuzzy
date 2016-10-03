8d458605366ad1867e4b2fd541788fe5b0e7f971
Oscar Holm <oscho707@student.liu,se>
2016-10-03 09:40:44 +0200

    afl loop

--- doc/afl/binarytest.cpp
+++ doc/afl/binarytest.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -17,26 +17,21 @@ int main(int argc, char** argv) {
     }
 
   char buffer[1000];
-  ifstream inputfile (argv[1], ios::in | ios::binary);
-
-  if (!inputfile)
-    {
-      cout << "FEL" << endl;
-      return 1;
-    }
+  stringstream ss;
   
+  ss << argv[1];
+  ss >> buffer;
+  //ifstream inputfile (argv[1], ios::in | ios::binary);
+
   parameters params;
-  inputfile.read(buffer, 1000);
   params = *((parameters*)buffer);
-
   ofstream outputfile ("outputfile.txt", ios_base::app);
-  //string str (buffer);
-  outputfile << "CYCLES: " << params.cycles << endl << "SEED: " << params.seed << endl << "RANDTYPE: " << (int)params.randtype << endl;
+  
+  while ( __AFL_LOOP(10) ) {
+    outputfile << "CYCLES: " << params.cycles << endl << "SEED: " << params.seed << endl << "RANDTYPE: " << (int)params.randtype << endl;
+  }
   outputfile.close();
-
   cout << buffer << endl;
-
-  inputfile.close();
   
   return 0;
 }
