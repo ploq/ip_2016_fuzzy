05d7db4a34d2c6258d41829ed04818a0faae335e
Tommy Lindman <tommy5545@gmail.com>
2016-10-17 15:41:20 +0200

    Added PortHandler.hpp.

---
+++ example/app_code/PortHandler.hpp
@@ -0,0 +1,23 @@
+#include <map>
+
+template<typename Value, typename Key>
+class PortHandler
+{
+public:
+   PortHandler() {}
+   ~PortHandler() {}
+
+   void* getPort(Key key)
+   {
+      if (m_ports.find(key) != m_ports.end())
+      {
+	 return m_ports[key];
+      } else
+      {
+	 m_ports[key] = Value();
+	 return m_ports[key];
+      }
+   }
+private:
+   std::map<Key, Value> m_ports;
+};
