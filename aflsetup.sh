946016cbd5cad535eb384ba48d61c63b938bdc34
Henrik Ingbrant Björs <henbj316@student.liu.se>
2016-11-07 08:17:53 +0100

    Moved export CC/CXX/etc to cbuild.sh

--- aflsetup.sh
+++
@@ -1,4 +0,0 @@
-export CC=/usr/bin/clang-3.8
-export CXX=/usr/bin/clang++-3.8
-export AFL_CC=/usr/bin/clang-3.8
-export AFL_CXX=/usr/bin/clang++-3.8
--- cbuild.sh
+++ cbuild.sh
@@ -1,6 +1,10 @@
+export CC=/usr/bin/clang-3.8
+export CXX=/usr/bin/clang++-3.8
+export AFL_CC=/usr/bin/clang-3.8
+export AFL_CXX=/usr/bin/clang++-3.8
 rm -rf build
 mkdir build
 cd build
 CXX=/home/pc022838/afl-2.35b/afl-clang-fast++
-cmake -D CMAKE_CXX_COMPILER=/home/pc022838/afl-2.35b/afl-clang-fast++ -std=gnu++03 ../example/ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake -D CMAKE_CXX_COMPILER=/home/inhuman/afl-2.33b/afl-clang-fast++ -std=gnu++03 ../example/ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 make
