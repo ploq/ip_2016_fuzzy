c8b66bf6462f09c113ea617dbe1a01add9578a43
Oscar Holm <oscar_holm94@hotmail.com>
2017-05-12 15:22:05 +0200

    Scripts for afl-cov setup and running.

---
+++ cbuild-gcov.sh
@@ -0,0 +1,8 @@
+AFL_CC=clang
+AFL_CXX=clang++
+rm -rf build_gcov
+mkdir build_gcov
+cd build_gcov
+CC=/home/oscho707/afl-2.35b/afl-clang-fast CXX=/home/oscho707/afl-2.35b/afl-clang-fast++ cmake -DCMAKE_CXX_FLAGS=" -fprofile-arcs -ftest-coverage" \
+-DCMAKE_EXE_LINKER_FLAGS="-fprofile-arcs -ftest-coverage" -std=c++11 ../flat/ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+make
