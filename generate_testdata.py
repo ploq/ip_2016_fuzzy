151be5d748cb7cf63baf0329b365ebe1d0dd9dbe
Oscar Holm <oscar_holm94@hotmail.com>
2017-05-18 09:57:12 +0200

    testfiles are now generated with python script instead of python3

--- generate_testdata.py
+++ generate_testdata.py
@@ -6,18 +6,18 @@ MAX_FILES = 5
 
 def generate_bytes(amount):
     """Generate 'amount' of random bytes"""
-    return [bytes([randint(1,255)]) for x in range(amount)]
+    return "".join([chr(randint(1,255)) for x in range(amount)])
 
 for i in range(MAX_FILES):
     with open ("./input/testdata_{}.txt".format(i), "wb") as f:
-        randtype = [bytes([randint(0,1)])]
+        randtype = bytes(chr(randint(0,1)))
         cycles = generate_bytes(randint(5,10))
         seed = generate_bytes(randint(6,15))
-        #print(randtype, cycles, seed)
+        print(randtype, cycles, seed)
         
-        data = randtype + [bytes([len(cycles)])] + cycles + [bytes([len(seed)])] + seed
-        data = [bytes([len(data)])] + data 
-        #print(repr(data))
-        #print(len(data))
+        data = randtype + bytes(chr(len(cycles))) + cycles + bytes(chr(len(seed))) + seed
+        data = bytes(chr(len(data))) + data 
+        print(repr(data))
+        print(len(data))
         f.write(b"".join(data))
     
